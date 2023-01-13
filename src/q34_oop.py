92d478ec51efa51d2a114732fbfbd6c284071ce9
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-01-13 11:27:42 +0900

    -z

--- src/q34_oop.py
+++ src/q34_oop.py
@@ -1,3 +1,24 @@
 class quiz34:
-    def__init__(self,num:int):
-        self
+    def __init__(self,num:int):
+        self.num=num
+        print("An object has been created")
+    def to_roman(self)->str:
+        num = self.num
+        if num > 100:
+            raise ValueError("Please input a number no greater than 100")
+        converted = ""
+        numbers,romans,i = [1, 4, 5, 9, 10, 40, 50, 90, 100],['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C'],8
+        while num:
+            div = num // numbers[i]
+            num %= numbers[i]
+            while div:
+                converted += str(romans[i])
+                div -= 1
+                if div == 0:
+                    break
+            i -= 1
+        return converted
+
+case1 = quiz34(num=1)
+solution = case1.to_roman()
+print(solution)
--- src/test_q34.py
+++ src/test_q34.py
@@ -1,11 +1,15 @@
-from q34 import to_roman
+import q34_oop
 import pytest
+#
+# def test_valid():
+#     assert to_roman(37)=='XXXVII'
+#     assert to_roman(44)=='XLIV'
+#     assert to_roman(100)=='C'
+#
+# def test_invalid():
+#     with pytest.raises(ValueError):
+#         to_roman(101)
 
-def test_valid():
-    assert to_roman(37)=='XXXVII'
-    assert to_roman(44)=='XLIV'
-    assert to_roman(100)=='C'
-
-def test_invalid():
-    with pytest.raises(ValueError):
-        to_roman(101)
+object1 = q34_oop.quiz34()
+def test_valid(object1):
+    assert object1.to_roman(num=37)=='XXXVII'
