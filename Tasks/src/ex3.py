37fd181404ad5470a6213c2945be3658a4d81a27
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-01-31 12:49:03 +0900

    -z

---
+++ Tasks/src/ex3.py
@@ -0,0 +1,11 @@
+from kivymd.app import MDApp
+
+class ex3(MDApp):
+    def build(self):
+        return
+
+    def change_author(self,name):
+        self.root.ids.title.text = f"Author {name}"
+
+test = ex3()
+test.run()
