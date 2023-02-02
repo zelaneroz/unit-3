8da98af3baac40452943ca90b141b7e1e2f716d8
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-02-02 13:42:54 +0900

    -z

---
+++ Quizzes/src/q41.py
@@ -0,0 +1,24 @@
+from kivymd.app import MDApp
+
+class q41(MDApp):
+    #def __init__(self, **kwargs):
+    def build(self):
+        return
+
+test = q41()
+test.run()
+#PSEUDOCODE
+
+# - MDBoxLayout - all contents
+# - 3x3 box
+# - MDLabel - Tic Tac Toe by student_name
+# - MDLabel - It is X's turn
+# - MDBoxLayout - vertical; 1 width; .3 height8
+#     - MDBoxLayout - horizontal; width is a .3
+#     - color of each raised button is first green.
+#
+# - My Add-ons (HL)
+#     - yellow & red for different turns;
+#     - change name of 2nd MDLabel for every turn.
+#     -
+
