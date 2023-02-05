03daf36d596d6b8fa78c54700535566c3212db8e
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-02-05 22:23:34 +0900

    -z

--- Quizzes/src/q40.py
+++ Quizzes/src/q40.py
@@ -3,17 +3,20 @@ import warnings
 warnings.simplefilter('ignore')
 
 class q40(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.colors = ["black","white","red","blue"]
     def build(self):
         return
-    def change_color(self):
-        if self.root.ids.main1.md_bg_color == "#FFFFFF":
-            self.root.ids.main1.md_bg_color = "#000000"
-            self.root.ids.btn1.md_bg_color = "#FF0000"
-            #self.root.ids.txt.text_color = "#FFFFFF"
+    def color(self):
+        if self.root.ids.main1.md_bg_color == self.colors[1]:
+            self.root.ids.main1.md_bg_color = self.colors[0]
+            self.root.ids.btn1.md_bg_color = self.colors[2]
+            self.root.ids.txt.text_color = self.colors[1]
         else:
-            self.root.ids.main1.md_bg_color = "#FFFFFF"
-            self.root.ids.btn1.md_bg_color = "#0000FF"
-            #self.root.ids.txt.text_color = "#000000"
+            self.root.ids.main1.md_bg_color = self.colors[1]
+            self.root.ids.btn1.md_bg_color = self.colors[3]
+            self.root.ids.txt.text_color = self.colors[0]
 
 test = q40()
 test.run()
