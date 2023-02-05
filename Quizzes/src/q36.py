727528a34115901640779f941e154d1ef5ed3e2b
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-02-05 20:27:54 +0900

    -z

--- Quizzes/src/q36.py
+++ Quizzes/src/q36.py
@@ -2,18 +2,21 @@ class Person:
     def __init__(self,name:str,age:int):
         self.name=name
         self.age=age
-
     def get_name(self):
+        if not isinstance(self.name,str):
+            raise ValueError("Name must be a string")
         return self.name
-
     def get_age(self):
+        if not isinstance(self.age, int):
+            raise ValueError("Please enter a whole number")
         return self.age
 
 class Student(Person):
     def __init__(self,name,age,grade):
         super().__init__(name,age)
         self.grade=grade
-
+        self.name=name
+        self.age=age
     def get_grade(self):
         return self.grade
 
@@ -25,12 +28,15 @@ class Classroom:
         self.students.append(student)
 
     def remove_students(self,student):
-        self.students.append(student)
+        if student not in self.students:
+            raise ValueError("Student not in classroom. Please enter a valid student.")
+        self.students.remove(student)
 
     def average_score(self):
-        students=self.students
+        if len(self.students)==0:
+            raise ValueError("Classroom is empty")
         avg_grade=0
-        for j in students:
+        for j in self.students:
             avg_grade+=j.get_grade()
-        avg_grade/=len(students)
+        avg_grade/=len(self.students)
         return avg_grade
--- Quizzes/src/test_q36.py
+++ Quizzes/src/test_q36.py
@@ -1,17 +1,34 @@
 import pytest
-from q36 import Person
-
-#add student
-#remove student
-#remove nonexisting student
-#average score
-#empty classroom
-#Value Errors
-#Type Errors
-# def test_add_student():
-#     cl=Classroom()
-#     assert cl.add_students('Kya')
+from q36 import Student,Classroom,Person
 
 def test_person():
-    pr=Person()
-    assert pr.name=''
+    p1=Person("Antonia",16)
+    assert p1.get_name()=="Antonia"
+    assert p1.get_age()==16
+    with pytest.raises(ValueError):
+        p2 = Person(17,"Zelan")
+        p2.get_age()
+        p2.get_name()
+def test_student():
+    student=Student("Sahana",17,85)
+    assert student.get_grade() == 85
+    assert student.get_name() == "Sahana"
+    assert student.get_age() == 17
+    with pytest.raises(ValueError):
+        student = Student(["Yasmina"],17,90)
+        student.get_name()
+
+def test_classroom():
+    c = Classroom()
+    s1 = Student("Mayte", 17,95)
+    s2 = Student("Lemie", 17, 84)
+    s3 = Student("Sana", 17, 87)
+    c.add_students(s1)
+    c.add_students(s2)
+    c.add_students(s3)
+    c.remove_students(s2)
+    assert c.average_score() == 91
+    c.remove_students(s1)
+    c.remove_students(s3)
+    with pytest.raises(ValueError):
+        c.average_score()
